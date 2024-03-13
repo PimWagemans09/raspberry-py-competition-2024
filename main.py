233eaad8db882e54d6575af1468b3ddf2ec24b82
pim <pimwagemans09@gmail.com>
2024-03-13 19:08:33 +0100

    added argparse

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import RPi.GPIO as gpio
-from gpiozero import MotionSensor
+import time
+import argparse
+import sys
+
+version = "0.1.0"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--just-version",action="store_true")
+args=parser.parse_args()
+if args.just_version:
+    print(version)
+    sys.exit(2)
 
 def mainloop():
     print(gpio.input(4))
+    if gpio.input(4) == gpio.HIGH:
+        print("motion detected!")
+        while gpio.input(4) == gpio.HIGH:
+            time.sleep(0.1)
+        print("motion stopped!")
+    time.sleep(0.2)
 
 def main():
     while True:
