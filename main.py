17891e286c889aae6c66ae283ec4b7c3f9e39ee1
pim <pimwagemans09@gmail.com>
2024-03-20 14:19:55 +0100

    commit

--- main.py
+++ main.py
@@ -20,38 +20,67 @@ if args.just_version:
     print(version)
     sys.exit(2)
 try:
-    temp , recording = read("lastsaved.wav")
+    temp , recording1 = read("lastsaved1.wav")
 except FileNotFoundError:
     samplerate = 16000; fs = 500
     t = np.linspace(0., 1., samplerate)
     amplitude = np.iinfo(np.int16).max
     data = amplitude * np.sin(2. * np.pi * fs * t)
-    write("lastsaved.wav", samplerate, data.astype(np.int16))
-    temp , recording = read("lastsaved.wav")
+    write("lastsaved1.wav", samplerate, data.astype(np.int16))
+    temp , recording1 = read("lastsaved1.wav")
 
-print(type(recording),type(temp))
+try:
+    temp , recording1 = read("lastsaved2.wav")
+except FileNotFoundError:
+    samplerate = 16000; fs = 500
+    t = np.linspace(0., 1., samplerate)
+    amplitude = np.iinfo(np.int16).max
+    data = amplitude * np.sin(2. * np.pi * fs * t)
+    write("lastsaved2.wav", samplerate, data.astype(np.int16))
+    temp , recording2 = read("lastsaved2.wav")
 
-def record():
-    global recording
+print(type(recording1),type(temp))
+
+def record1():
+    global recording1
+    recording1 = sounddevice.rec(int(DURATION*16000),samplerate=16000,channels=2)
+    sounddevice.wait()
+    os.remove("lastsaved1.wav")
+    write("lastsaved1.wav",16000,recording1)
+
+def play1():
+    global recording1
+    sounddevice.play(recording1,samplerate=16000)
+    sounddevice.wait()
+
+def record2():
+    global recording2
     recording = sounddevice.rec(int(DURATION*16000),samplerate=16000,channels=2)
     sounddevice.wait()
-    os.remove("lastsaved.wav")
-    write("lastsaved.wav",16000,recording)
+    os.remove("lastsaved2.wav")
+    write("lastsaved2.wav",16000,recording)
 
-def play():
-    global recording
-    sounddevice.play(recording,samplerate=16000)
+def play2():
+    global recording2
+    sounddevice.play(recording2,samplerate=16000)
     sounddevice.wait()
 
 def mainloop():
-    print(f"play: {gpio.input(4)} | record: {gpio.input(17)}")
+    #print(f"play: {gpio.input(27)} | record: {gpio.input(22)}")
     if gpio.input(4) == gpio.HIGH:
-        play()
-        print("playing audio")
+        print("playing audio 1")
+        play1()
     if gpio.input(17) == gpio.HIGH:
-        print("started recording")
-        record()
-        print("recording finished")
+        print("started recording 1")
+        record1()
+        print("recording finished 1")
+    if gpio.input(27) == gpio.HIGH:
+        print("playing audio 2")
+        play2()
+    if gpio.input(22) == gpio.HIGH:
+        print("started recording 2")
+        record2()
+        print("recording finished 2")
     time.sleep(0.2)
 
 def main():
@@ -62,6 +91,8 @@ try:
     gpio.setmode(gpio.BCM)
     gpio.setup(4,gpio.IN,pull_up_down=gpio.PUD_DOWN)
     gpio.setup(17,gpio.IN)
+    gpio.setup(27,gpio.IN)
+    gpio.setup(22,gpio.IN)
     if __name__ == "__main__":
         main()
 finally:
